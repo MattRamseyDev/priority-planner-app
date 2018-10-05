cf46d159e715e78dfcf1f76e8d20bdcc3a328cb6
Matt Ramsey <m.ramsey1995@gmail.com>
2018-10-05 11:52:34 -0500

    will update existing todo instead of adding a duplicate todo

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -6,6 +6,18 @@ const todoReducerDefaultState = [];
 export default (state = todoReducerDefaultState, action) => {
     switch (action.type) {
         case 'ADD_TODO':
+            // checks if the todo already exists in another list
+                // if it does, add update lists of existing todo instead of creating a new one
+            let match = state.find(todo => todo.description === action.todo.description);
+            if (match) {
+                match.lists = match.lists.concat(action.todo.lists); // currently will add duplicates of list items
+                state.map((todo) => {
+                    if (todo.id === match.id){
+                        return {...match};
+                    }
+                });
+                return [...state]
+            }
             return [...state, action.todo];
         case 'REMOVE_TODO':
             return state.filter(todo => todo.id !== action.id);

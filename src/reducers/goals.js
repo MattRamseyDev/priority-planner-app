97c52f60be8247373edcd87c319af8265d527f7e
Matt Ramsey <m.ramsey1995@gmail.com>
2018-10-09 11:10:07 -0500

    create goals reducer

---
+++ src/reducers/goals.js
@@ -0,0 +1,9 @@
+
+const goalsReducerDefaultState = [];
+
+export default (state= goalsReducerDefaultState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}

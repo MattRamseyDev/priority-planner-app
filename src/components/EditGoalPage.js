ca323d66e83b80069b46409acded8bf6a038facb
Matt Ramsey <m.ramsey1995@gmail.com>
2018-10-10 10:24:53 -0500

    connect EditGoalPage to store and display goal information

--- src/components/EditGoalPage.js
+++ src/components/EditGoalPage.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const EditGoalPage = () => (
-    <div>
-        wassup dud
-    </div>
-);
+const EditGoalPage = (props) => {
+    {console.log(props)}
+    return (
+        <div>
+            <p>{props.goal.description}</p>
+        </div>
+    );
+}
 
-export default EditGoalPage;
+const mapStateToProps = (state, props) => ({
+    goal: state.goals.find((goal) => goal.id === props.match.params.id) // undefined after refresh
+})
+
+export default connect(mapStateToProps)(EditGoalPage);

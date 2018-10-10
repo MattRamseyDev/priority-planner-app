dcd56ed9ac3e2252f449bc01f065c09c99c1a4c5
Matt Ramsey <m.ramsey1995@gmail.com>
2018-10-10 09:24:14 -0500

    create addGoal component

---
+++ src/components/AddGoal.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+export default class AddGoal extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: ''
+        };
+    }
+    onSubmit = (e) => {
+        e.preventDefault();
+        const newGoal = e.target.elements.goal.value.trim();
+        if (!newGoal) {
+            this.setState(() => ({ error: 'Please enter a new goal' }))
+        } else {
+            this.props.onSubmit(newGoal);
+            this.setState(() => ({ error: '' }));
+            e.target.elements.goal.value = '';
+        }
+    }
+    render() {
+        return (
+            <div>
+            <form onSubmit={this.onSubmit}>
+                <input type='text' name='goal'></input>
+                <button>Add Goal</button>
+            </form>
+            {this.state.error && <p>{this.state.error}</p>}
+            </div>
+        )
+    }
+}
--- src/components/GoalPage.js
+++ src/components/GoalPage.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import GoalList from './GoalList';
+import AddGoal from './AddGoal';
+import { addGoal } from '../actions/goals';
 
 const GoalPage = (props) => (
     <div>
-        <h2>What are you trying to do?</h2>
+        <h2>What do you want to do?</h2>
         <GoalList />
+        <AddGoal onSubmit={(newGoal) => props.dispatch(addGoal(newGoal))}/>
     </div>
 )
 

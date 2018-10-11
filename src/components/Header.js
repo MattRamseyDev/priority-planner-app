97fc0d34e56e57c49ea1af7041d7db789c981727
Matt Ramsey <m.ramsey1995@gmail.com>
2018-10-11 11:11:50 -0500

    add goals to header to avoid refresh problem

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,7 @@ const Header = () => (
         <h1>Priority Planner</h1>
         <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard   </NavLink>
         <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+        <NavLink to='/goals' activeClassName='is-active'>Goals</NavLink>
     </header>
 );
 

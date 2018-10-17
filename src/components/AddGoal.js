5e9a68a7807791abaa4698a421c230de433906ad
Matt Ramsey <m.ramsey1995@gmail.com>
2018-10-17 11:42:34 -0500

    Render AddGoal component for NewGoalPage instead of GoalForm

--- src/components/AddGoal.js
+++ src/components/AddGoal.js
@@ -4,14 +4,14 @@ export default class AddGoal extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            error: ''
+          error: ''
         };
     }
     onSubmit = (e) => {
         e.preventDefault();
         const newGoal = e.target.elements.goal.value.trim();
         if (!newGoal) {
-            this.setState(() => ({ error: 'Please enter a new goal' }))
+            this.setState(() => ({ error: 'Please enter a goal description' }))
         } else {
             this.props.onSubmit(newGoal);
             this.setState(() => ({ error: '' }));
--- src/components/NewGoalPage.js
+++ src/components/NewGoalPage.js
@@ -2,15 +2,22 @@ import React from 'react';
 import {connect} from 'react-redux';
 import GoalForm from './GoalForm';
 import { addGoal } from '../actions/goals';
+import AddGoal from './AddGoal';
 
 const NewGoalPage = (props) => (
     <div>
-        <GoalForm 
+      <AddGoal 
+        onSubmit={(description) => {
+          const newGoal = props.dispatch(addGoal(description));
+          props.history.push(`/goals/${newGoal.goal.id}`)
+        }}
+      />
+        {/* <GoalForm 
             onSubmit={({description}) => {
                 const newGoal = props.dispatch(addGoal(description));
                 props.history.push(`/goals/${newGoal.goal.id}`)
             }}
-        />
+        /> */}
     </div>
 );
 
